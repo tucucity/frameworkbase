938667da8c2a43054af5e54970bad54214405fc5
bautistalf <bautista.luis.88@gmail.com>
2015-09-03 09:07:51 -0300

    Configuracion del archivo de configuracion

--- config/config.php
+++ config/config.php
@@ -2,13 +2,13 @@
 //Config -----
 $_config = array(
     //Host Configuration
-    'PROYECTO'=>'SimplePHP!',
-    'HOME'=>'http://localhost/simplephp/',
+    'PROYECTO'=>'FrameworkBase!',
+    'HOME'=>'http://localhost:81/frameworkbase/',
     //Database Configuration
-    'DB_HOST'=>'localhost',
-    'DB_USER'=>'root',
-    'DB_PASS'=>'root',
-    'DB_NAME'=>'MiDB',
+    'DB_HOST'=>'javiercani.net',
+    'DB_USER'=>'javierca_luis',
+    'DB_PASS'=>'31i7331i73',
+    'DB_NAME'=>'javierca_tucucity',
     'DB_PORT'=>3306,
     //Cantidad de / a leer en la URL
     'CONT'=>2
